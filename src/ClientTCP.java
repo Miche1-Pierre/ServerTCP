dd6b6f56962fa3e453eb0ef7c283ca316dc0a22d
Miche1-Pierre <pierre.michel.work@gmail.com>
2025-04-23 18:19:14 +0200

    feat: added ClientTCP file

--- src/ClientTCP.java
+++ src/ClientTCP.java
@@ -1,2 +1,27 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
 public class ClientTCP {
+    public static void main(String[] args) throws IOException {
+        try {
+            Socket clientSocket = new Socket("localhost", 8585);
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter writer = new PrintWriter(clientSocket.getOutputStream(), true);
+
+            writer.println("Salut ! (Client)");
+            System.out.println(reader.readLine());
+
+            reader.close();
+            writer.close();
+            clientSocket.close();
+
+        }
+        catch (Exception e) {
+            System.out.println("[ERREUR] : " + e);
+        }
+    }
 }
